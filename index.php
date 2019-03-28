13bfcb5dc2515231d8b04ce228735949a6fec1e0
Jaskaran111999 <jaskaran110398@gmail.com>
2019-03-29 04:17:37 +1100

    app setup successful

--- index.php
+++ index.php
@@ -5,21 +5,12 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>Simple Database App</title>
-
     <link rel="stylesheet" href="css/style.css" />
   </head>
 
   <body>
-    <h1>Simple Database App</h1>
-
-    <ul>
-      <li>
-        <a href="create.php"><strong>Create</strong></a> - add a user
-      </li>
-      <li>
-        <a href="read.php"><strong>Read</strong></a> - find a user
-      </li>
+    <h1>Admin Role</h1>
+		<p>Hello World!!</p>
     </ul>
   </body>
 </html>
