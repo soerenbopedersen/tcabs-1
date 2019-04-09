dad0aa408eccbe40708f9cb09e16bed651d7c8c0
Jaskaran111999 <jaskaran110398@gmail.com>
2019-04-10 00:55:00 +1000

    DDL and data insertion for enrolment

---
+++ dataInsertion.sql
@@ -0,0 +1,11 @@
+INSERT INTO Users VALUES (1, "Daenerys", "Targaryen", "F", "dtargaryen@gmail.com", "motherofdragons");
+INSERT INTO Users VALUES (2, "Tyrion", "Lannister", "M", "tlannister@gmail.com", "lannisteralwayspaysitsdebt");
+INSERT INTO Users VALUES (1, "John", "Snow", "M", "jsnoe@gmail.com", "kingingthenorth");
+INSERT INTO Users VALUES (1, "Robert", "Baratheon", "M", "rbaratheon@gmail.com", "rulerofsevenkingdoms");
+
+INSERT INTO UserRole VALUES (1, "Admin");
+INSERT INTO UserRole VALUES (2, "Convenor");
+INSERT INTO UserRole VALUES (3, "Supervisor");
+INSERT INTO UserRole VALUES (4, "Student");
+
+-- INSERT INTO UserUserRole VALUES (1, "John", "Snow", "M", "jsnoe@gmail.com", "kingingthenorth");
