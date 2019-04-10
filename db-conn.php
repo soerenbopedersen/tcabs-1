2ebe48ff78d7bfce26ee23da8f72248556d183bd
Jaskaran111999 <jaskaran110398@gmail.com>
2019-04-10 20:54:30 +1000

    Enrolment output on homepage

--- db-conn.php
+++ db-conn.php
@@ -1,17 +1,40 @@
 <?php
-  $conn = new mysqli("139.99.166.3", "tcabs_web", "Database1", "tcabs");
+
+	//main server
+  //$conn = new mysqli("139.99.166.3", "tcabs_web", "Database1", "tcabs");
+
+	//local server
+  $conn = new mysqli("127.0.0.1", "root", "", "tcabs");
   
   if ($conn->connect_error) {
     die("ERROR: Unable to connect: " . $conn->connect_error);
   } 
 
-  echo 'Connected to the database.<br>';
+  echo 'Connected to the database.<br><br>';
+	echo 'Listing all student enrolments!!<br><br>';
+	echo '-------------------------------------------------------------------------------<br><br>';
 
-  $result = $conn->query("SELECT * FROM SUBJECTS");
+	echo '| EnrolmentID | StudentID | StudenName | UnitCode | UnitName | UnitPeriod |<br>';
 
-  echo "Number of rows: $result->num_rows";
+	$mysql = "select Enrolment.enrolmentID, Student.studentID, Users.fName, Users.lName, Unit.unitCode, Unit.unitName, UnitOffering.period
+						from Enrolment
+						inner join Student on Student.studentID = Enrolment.studentID
+						inner join UserUserRole on UserUserRole.uurID = Student.uurID
+						inner join Users on Users.userID = UserUserRole.uurID
+						inner join UnitOffering on UnitOffering.unitOfferingID = Enrolment.unitOfferingID
+						inner join Unit on Unit.unitID = UnitOffering.unitID";
 
-  $result->close();
+  $result = $conn->query($mysql);
+
+	if ($result->num_rows > 0) {
+
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "$row[enrolmentID].$row[studentID].$row[fName].$row[lName].$row[unitCode].$row[unitName].$row[period].<br>";
+    }
+	} else {
+    echo "0 results";
+	}
 
   $conn->close();
 ?>
