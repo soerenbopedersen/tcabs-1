610e16c9177d16bfcfe5f563ee5eb3a0e5aadca5
Jaskaran111999 <jaskaran110398@gmail.com>
2019-05-01 23:33:31 +1000

    Add Admin Data

--- dataInsertion.sql
+++ dataInsertion.sql
@@ -2,7 +2,7 @@ INSERT INTO tcabs.Users VALUES (1, "Daenerys", "Targaryen", "F", "0412323443", "
 INSERT INTO tcabs.Users VALUES (2, "Tyrion", "Lannister", "M", "0412332543", "tlannister@gmail.com", "lannisteralwayspaysitsdebt");
 INSERT INTO tcabs.Users VALUES (3, "John", "Snow", "M", "0412332243", "jsnow@gmail.com", "kingingthenorth");
 INSERT INTO tcabs.Users VALUES (4, "Robert", "Baratheon", "M", "0412332263", "rbaratheon@gmail.com", "rulerofsevenkingdoms");
-INSERT INTO tcabs.Users VALUES (5, "Arya", "Start", "F", "0412332263", "astark@gmail.com", "thereisonlyonegod");
+INSERT INTO tcabs.Users VALUES (5, "Arya", "Stark", "F", "0412332263", "astark@gmail.com", "thereisonlyonegod");
 
 INSERT INTO tcabs.UserRole VALUES (1, "Admin");
 INSERT INTO tcabs.UserRole VALUES (2, "Convenor");
@@ -14,6 +14,8 @@ INSERT INTO tcabs.UserUserRole VALUES (2, 1, 1);
 INSERT INTO tcabs.UserUserRole VALUES (3, 5, 4);
 INSERT INTO tcabs.UserUserRole VALUES (4, 4, 2);
 INSERT INTO tcabs.UserUserRole VALUES (5, 3, 2);
+
+INSERT INTO tcabs.Admin VALUES (1, 2);
 
 INSERT INTO tcabs.Unit VALUES (1, "ICT30001", "Information Technology Project", "FSET");
 INSERT INTO tcabs.Unit VALUES (2, "INF30011", "Database Implementation", "FSET");
