26b2f95df9cadf9efc618ee99ec449dcf7089b58
Shannon Savage <sshannonau@gmail.com>
2019-05-09 11:48:15 +1000

    Change Header/Navbar to Bootstrap
    Created bootstrap header for easy to create drop down menus, responsive layout etc.

--- header.php
+++ header.php
@@ -1,9 +1,33 @@
-<nav class="navtop">
-	<div>
-		<h1>Team Contribution and Budget System</h1>
-		<!--Check $_SESSION['UserRole'] and display Admin/Student etc -->
-		<a href="admin.php"><i class="fas fa-user-secret"></i>Insert User</a>
-		<a href="profile.php"><i class="fas fa-user-circle"></i>Profile (<?php echo $_SESSION['fName']?>)</a>
-		<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-	</div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Team Contribution and Budget System</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="dashboard.php">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="profile.php">Profile</a>
+      </li>
+	  <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-user-secret"></i> Admin
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="registerUser.php">Add Users</a>
+          <a class="dropdown-item" href="#">Add Units</a>
+          <a class="dropdown-item" href="#">Enrol Students</a>
+        </div>
+      </li>
+	  
+	  <li class="nav-item">
+        <a class="nav-link" href=logout.php">Logout</a>
+      </li>
+	  
+    </ul>
+      <?php echo date(' (l) d F Y h:ia'); ?>
+    </span>
+  </div>
 </nav>
