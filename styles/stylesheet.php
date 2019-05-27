9dcee2079491fe18db7c49f44b7773cec2d5a3ea
soerenbopedersen <48793885+soerenbopedersen@users.noreply.github.com>
2019-05-27 17:54:52 +1000

    Update stylesheet.php
    Additional layout for tables and buttons - relevant for reporting tasks

--- styles/stylesheet.php
+++ styles/stylesheet.php
@@ -5,3 +5,45 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th, td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+
+.btn-group-justified {
+  display: table;
+  width: 100%;
+  table-layout: fixed;
+  border-collapse: separate; }
+  .btn-group-justified .btn,
+  .btn-group-justified .btn-group {
+  float: none;
+  display: table-cell;
+  width: 1%; }
+  .btn-group-justified .btn .btn,
+  .btn-group-justified .btn-group .btn {
+  width: 100%; }
+  .btn-group-justified .btn .dropdown-menu,
+  .btn-group-justified .btn-group .dropdown-menu {
+  left: auto; }
+
+ input {
+   background-color: #eee;
+   border: none;
+   padding: 3px 3px;
+   margin: 3px 3px;
+   width: 100%;
+   position: relative;
+   text-align: center;
+   }
+
+tr:hover {background-color:#f5f5f5;}
+</style>
